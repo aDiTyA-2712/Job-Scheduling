80847212e56c2dcf89a4da95e8fb417fafccf2b5
aDiTyA-2712 <adityakumar2798@gmail.com>
2024-11-24 01:15:10 +0530

    adding requiremnts

--- scheduler_proj/celery.py
+++ scheduler_proj/celery.py
@@ -1,11 +1,14 @@
 # scheduler_proj/celery.py
 
 from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
+import os                       #way of using operating system dependent functionality
+from celery import Celery       #imports 'Celery' class from 'celery' module 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler_proj.settings')
+#sets an environment variable to 'scheduler_proj.settings' , This is necessary because Celery 
+#Celery needs to know where to find the Django project's settings.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheduler_proj.settings') 
 
-app = Celery('scheduler_proj')
+#creates a new Celery application instance, passing the name of the project ('scheduler_proj') to it                                                                           #needs to know where to find the Django project's settings.
+app = Celery('scheduler_proj')      
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
